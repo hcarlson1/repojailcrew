1997120aa04ad95a3ab3c19d881fd6f3b93de6cf
Heather <hcarlson1@css.edu>
2017-06-15 10:08:41 -0500

    Database and major HTML changes. everything working up to this point.

---
+++ WebApplication1/DAL/TestDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.DAL
+{
+    public class TestDBContext : DbContext
+    {
+        public TestDBContext() : base("TestConnection")
+        {
+
+        }
+        public DbSet<Test> Test { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Test
+    {
+        
+        [Key]
+        public int TestId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
